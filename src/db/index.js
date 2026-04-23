2f36d2421606ceb9eee1e5deaac733edb4e0de09
yagyam <vishuaggarwal219@gmail.com>
2026-04-24 01:57:32 +0530

    refactor database connection logic to conditionally load dotenv in non-production environments

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,12 @@
-import dotenv from "dotenv"
-dotenv.config()
+if (process.env.NODE_ENV !== "production") {
+    import("dotenv").then(dotenv => dotenv.config());
+}
 import mongoose from "mongoose"
-import { DB_NAME } from "../constants.js"
+// import { DB_NAME } from "../constants.js"
 
 export const dbConnect = async ()=> {
     try {
-        const connectionString = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionString = await mongoose.connect(`${process.env.MONGODB_URI}`)
     
         console.log("DB connected: ", connectionString.connection.host);
     } catch (error) {
